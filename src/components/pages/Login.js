a6799d57b0b77eb529906450994530895fbc6dd4
Ramish-Amir <49115590+Ramish-Amir@users.noreply.github.com>
2021-04-26 14:06:31 +0500

    Completed Login/signup form binding

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 import './Login.css'
-import Button from "../Button";
 
 const Login = () => {
+    const [loginForm, setLoginForm] = useState({
+        username: '',
+        password: ''
+    })
+
     const loginUser = () => {
         console.log('Logged in')
+        console.log(JSON.stringify(loginForm))
     }
+
     return (
         <>
             <div className={'login-container'}>
@@ -17,16 +23,23 @@ const Login = () => {
                     <div className={'card-content'}>
                         <div className={'row'}>
                             <div className={'input-field'}>
-                                <input type={'text'} placeholder={'Username'}/>
+                                <input type={'text'} placeholder={'Username'} value={loginForm.username || ''}
+                                       onChange={
+                                           e => (setLoginForm({...loginForm, username: e.target.value}))
+                                       }/>
                                 <i className="far fa-user"/>
                             </div>
                             <div className={'input-field'}>
-                                <input type={'password'} placeholder={'Password'}/>
+                                <input type={'password'} placeholder={'Password'} value={loginForm.password || ''}
+                                       onChange={
+                                           e => (setLoginForm({...loginForm, password: e.target.value}))
+                                       }/>
                                 <i className="fas fa-lock"/>
                             </div>
                         </div>
                         <div className={'login-button'}>
-                            <button className={'btn btn--medium btn--outline'} onClick={loginUser}>Login</button>
+                            <button className={'btn btn--medium btn--outline'} onClick={loginUser}><i
+                                className="fas fa-arrow-right"/></button>
                         </div>
                     </div>
                 </div>
--- src/components/pages/Signup.js
+++ src/components/pages/Signup.js
@@ -6,9 +6,10 @@ const Signup = () => {
     const [step, setStep] = useState(1)
     const [signupForm, setSignupForm] = useState({
         userType: '',
-        name: '',
+        name: null,
         email: '',
         phNumber: '',
+        password: '',
         area: '',
         address: '',
         ntn: '',
@@ -17,12 +18,21 @@ const Signup = () => {
         regAddress: ''
     })
 
+    const openMap = () => {
+        console.log('Address Input clicked')
+    }
+
     const handleUserType = (type) => {
         setSignupForm({
             userType: type
         })
         setStep(2)
         console.log(signupForm.userType)
+    }
+
+    const submitBioData = () => {
+
+        console.log(JSON.stringify(signupForm))
     }
 
     const nextForm = () => {
@@ -44,56 +54,67 @@ const Signup = () => {
                     </div>
 
                     {step === 1 && <div className={'card-content'}>
-                        <Button buttonStyle={'btn--outline'} onClick={() => handleUserType('customer')}>Sign up as Customer <i
-                            className="fas fa-chevron-right"/></Button>
-                        <Button buttonStyle={'btn--outline'} onClick={() => handleUserType('corporate')}>Sign up as Corporate <i
-                            className="fas fa-chevron-right"/></Button>
-                        <Button buttonStyle={'btn--outline'} onClick={() => handleUserType('employee')}>Sign up as Employee <i
-                            className="fas fa-chevron-right"/></Button>
+                        <Button buttonStyle={'btn--outline'} onClick={() => handleUserType('customer')}>Sign up as
+                            Customer <i
+                                className="fas fa-chevron-right"/></Button>
+                        <Button buttonStyle={'btn--outline'} onClick={() => handleUserType('corporate')}>Sign up as
+                            Corporate <i
+                                className="fas fa-chevron-right"/></Button>
+                        <Button buttonStyle={'btn--outline'} onClick={() => handleUserType('employee')}>Sign up as
+                            Employee <i
+                                className="fas fa-chevron-right"/></Button>
                     </div>}
 
                     {step === 2 && <div className={'card-content'}>
                         <div className={'row'}>
                             <div className={'input-field'}>
-                                <input type={'text'} placeholder={'Name'}/>
+                                <input type={'text'} placeholder={'Name'} value={signupForm.name || ''}
+                                       onChange={e => setSignupForm({...signupForm, name: e.target.value})}/>
                                 <i className="far fa-user"/>
                             </div>
                             <div className={'input-field'}>
-                                <input type={'email'} placeholder={'Email'}/>
+                                <input type={'email'} placeholder={'Email'} value={signupForm.email || ''}
+                                       onChange={e => setSignupForm({...signupForm, email: e.target.value})}/>
                                 <i className="far fa-envelope"/>
                             </div>
                         </div>
                         <div className={'row'}>
                             <div className={'input-field'}>
-                                <input type={'text'} placeholder={'Phone'}/>
+                                <input type={'text'} placeholder={'Phone'} value={signupForm.phNumber || ''}
+                                       onChange={e => setSignupForm({...signupForm, phNumber: e.target.value})}/>
                                 <i className="fas fa-phone"/>
                             </div>
                             <div className={'input-field'}>
-                                <input type={'password'} placeholder={'Password'}/>
+                                <input type={'password'} placeholder={'Password'} value={signupForm.password || ''}
+                                       onChange={e => setSignupForm({...signupForm, password: e.target.value})}/>
                                 <i className="fas fa-lock"/>
                             </div>
                         </div>
                         <div className={'row'}>
                             <div className={'input-field'}>
                                 <div>
-                                    <select>
-                                        <option disabled selected hidden>Area</option>
-                                        <option value={'Jinnah Garden, Islamabad'}>Jinnah Garden, Islamabad</option>
+                                    <select value={signupForm.area || ''}
+                                            onChange={e => setSignupForm({...signupForm, area: e.target.value})}>
+                                        {/*<option disabled defaultValue={'Area'} hidden>Area</option>*/}
+                                        <option defaultChecked value={'Jinnah Garden, Islamabad'}>Jinnah Garden,
+                                            Islamabad
+                                        </option>
                                         <option value={'River Garden, Islamabad'}>River Garden, Islamabad</option>
-                                        <option value={'wan Garden, Islamabad'}>Swan Garden, Islamabad</option>
+                                        <option value={'Swan Garden, Islamabad'}>Swan Garden, Islamabad</option>
                                     </select>
                                 </div>
                                 <i className="far fa-address-card"/>
                             </div>
                             <div className={'input-field'}>
-                                <input type={'text'} placeholder={'Address'}/>
+                                <input type={'text'} placeholder={'Address'} onFocus={openMap}/>
                                 <i className="fas fa-map-marker-alt"/>
                             </div>
                         </div>
                         <div className={'row button'}>
                             <Button buttonStyle={'btn--outline'} onClick={prevForm}><i
                                 className="fas fa-arrow-left"/></Button>
-                            <Button buttonStyle={'btn--outline'} onClick={nextForm}><i className="fas fa-arrow-right"/></Button>
+                            <Button buttonStyle={'btn--outline'} onClick={submitBioData}><i
+                                className="fas fa-arrow-right"/></Button>
                         </div>
                     </div>}
 
